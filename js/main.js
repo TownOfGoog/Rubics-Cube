5b84083155c057af65f1ae4a19b14cf7c9936401
TownOfGoog <95412264+TownOfGoog@users.noreply.github.com>
2023-05-18 12:59:17 +0200

    Rotation limit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const three = new Threestrap.Bootstrap();
+//import * as POSTPROCESSING from 'postprocessing';
 import { setCubeColors } from './cubes.js';
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
 
 
 
@@ -14,6 +19,8 @@ for (var i = 1; i <= 28; i++) {
   cubes.push(cube);
 }
 
+
+
 setCubeColors(cubes);
 three.scene.add(cube);
 three.camera.position.set(1, 1, 8);
@@ -21,13 +28,15 @@ three.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 const cubeGroup = new THREE.Group();
 let num = 1;
+
+
 const materials = [
   new THREE.MeshBasicMaterial({ color: 0xf5b7b1 }), // Front face (Pastel pink)
   new THREE.MeshBasicMaterial({ color: 0xb2dbbf }), // Back face (Pastel green)
   new THREE.MeshBasicMaterial({ color: 0xaed6f1 }), // Top face (Pastel blue)
   new THREE.MeshBasicMaterial({ color: 0xf9e79f }), // Bottom face (Pastel yellow)
   new THREE.MeshBasicMaterial({ color: 0xd7bde2 }), // Right face (Pastel purple)
-  new THREE.MeshBasicMaterial({ color: 0x82e0aa })  // Left face (Pastel turquoise)
+  new THREE.MeshBasicMaterial({ color: 0xffffff })  // Left face (Pastel turquoise)
 ];
 
 
@@ -49,12 +58,18 @@ for (var q = -1; q <= 1; q++) {
   cubes[14].material = material
 }
 
-var geometry = new THREE.BoxGeometry(0.9,0.9, 0.9);
-var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-var c = new THREE.Mesh(geometry, material);
+var testgeometry = new THREE.BoxGeometry(0.9,0.9, 0.9);
+var testmaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
 
-three.scene.add(c)
-c.position.set(0,0,4)
+var edges = new THREE.EdgesGeometry(testgeometry);
+edges.scale(0.95, 0.95, 0.95);
+
+var testcube = new THREE.Mesh(testgeometry, testmaterial);
+
+three.scene.add(testcube)
+testcube.position.set(0,0,4)
+
+
 
 
 
@@ -63,24 +78,6 @@ c.position.set(0,0,4)
 
 three.scene.add(cubeGroup)
 
-document.addEventListener('mousemove', onMouseMove, false);
-
-function onMouseMove(event) {
-  const mouse = new THREE.Vector2();
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-  const raycaster = new THREE.Raycaster();
-  raycaster.setFromCamera(mouse, three.camera);
-
-  const intersects = raycaster.intersectObject(c);
-
-  if (intersects.length > 0) {
-    c.material.color.set(0xff0000); // Set red color
-  } else {
-    c.material.color.set(0xffffff); // Set original color
-  }
-}
 
 
 
@@ -111,7 +108,10 @@ function onMouseMove(event) {
     const deltaY = event.clientY - previousMouseY;
 
     cubeGroup.rotation.y += (deltaX * rotationSpeed);
-    cubeGroup.rotation.x += (deltaY * rotationSpeed);
+
+    // Restrict rotation along the X axis
+    const newXRotation = cubeGroup.rotation.x + (deltaY * rotationSpeed);
+    cubeGroup.rotation.x = Math.max(-Math.PI / 4, Math.min(Math.PI / 4, newXRotation));
 
     previousMouseX = event.clientX;
     previousMouseY = event.clientY;
@@ -124,7 +124,24 @@ function onMouseUp(event) {
   }
 }
 
-three.on('update', function () {
-  // Add any additional animation or update logic here
-});
 
+
+//const composer = new POSTPROCESSING.EffectComposer(renderer);
+//composer.addPass(new POSTPROCESSING.RenderPass(scene, camera));
+//
+//const pixelationEffect = new POSTPROCESSING.PixelationEffect();
+//composer.addPass(pixelationEffect);
+//
+//pixelationEffect.resolution.set(512, 512);
+//pixelationEffect.pixelSize = 8.0;
+
+document.addEventListener("mouseover", mouseOverCube, false)
+
+
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  composer.render();
+}
+animate();
