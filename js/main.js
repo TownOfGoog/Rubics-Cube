ea6ea7e7bcbc9f75fb6e51fd5c98f2a75785adeb
TownOfGoog <95412264+TownOfGoog@users.noreply.github.com>
2023-05-25 08:34:27 +0200

    Erster versuch fuer Hitboxen

--- js/main.js
+++ js/main.js
@@ -1,34 +1,30 @@
+import { mainRotation } from './mainRotation.js';
 const three = new Threestrap.Bootstrap();
-//import * as POSTPROCESSING from 'postprocessing';
-import { setCubeColors } from './cubes.js';
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-
 
 
 var cubes = [];
 
 for (var i = 1; i <= 28; i++) {
-  var geometry = new THREE.BoxGeometry(0.9,0.9, 0.9);
+  var geometry = new THREE.BoxGeometry(0.9, 0.9, 0.9);
   var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
   var cube = new THREE.Mesh(geometry, material);
-  
+
   cube.name = "cube" + i;
   cubes.push(cube);
 }
+var geometry = new THREE.BoxGeometry(2.8, 2.8, 2.8);
+var material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 });
+var rubicsCubeHitbox = new THREE.Mesh(geometry, material);
+rubicsCubeHitbox.visible = false;
 
 
+three.scene.add(cubes);
 
-setCubeColors(cubes);
-three.scene.add(cube);
-three.camera.position.set(1, 1, 8);
+three.camera.position.set(0, 0, 8);
 three.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 const cubeGroup = new THREE.Group();
 let num = 1;
-
 
 const materials = [
   new THREE.MeshBasicMaterial({ color: 0xf5b7b1 }), // Front face (Pastel pink)
@@ -39,109 +35,97 @@ const materials = [
   new THREE.MeshBasicMaterial({ color: 0xffffff })  // Left face (Pastel turquoise)
 ];
 
-
-
 for (var q = -1; q <= 1; q++) {
   for (var w = -1; w <= 1; w++) {
     for (var e = -1; e <= 1; e++) {
       three.scene.add(cubes[num])
-      if(q!=2&&w!=2&&e!=2){
-      cubes[num].material = materials
+      if (q != 2 && w != 2 && e != 2) {
+        cubes[num].material = materials
 
-      cubes[num].position.set(q,w,e);
-      cubeGroup.add(cubes[num])
-      num++;
-        
+        cubes[num].position.set(q, w, e);
+        cubeGroup.add(cubes[num])
+        num++;
+
       }
     }
   }
-  cubes[14].material = material
+}
+cubeGroup.add(rubicsCubeHitbox)
+
+
+
+
+var geometry = new THREE.BoxGeometry(2.9, 0.1, 2.9);
+var material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 });
+var rubicsCubeHitboxTop = new THREE.Mesh(geometry, material);
+rubicsCubeHitboxTop.visible = false;
+rubicsCubeHitboxTop.position.set(0,0,0)
+cubeGroup.add(rubicsCubeHitboxTop)
+
+
+var material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 });
+var rubicsCubeHitboxBottom = new THREE.Mesh(geometry, materials);
+rubicsCubeHitboxBottom.visible = true;
+rubicsCubeHitboxBottom.position.set(0,1.5,0)
+cubeGroup.add(rubicsCubeHitboxBottom)
+
+var geometry = new THREE.BoxGeometry(2.8, 2.8, 2.8);
+var material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 });
+var rubicsCubeHitboxNorth = new THREE.Mesh(geometry, material);
+rubicsCubeHitboxNorth.visible = false;
+
+var material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 });
+var rubicsCubeHitboxWest = new THREE.Mesh(geometry, material);
+rubicsCubeHitboxWest.visible = false;
+
+var geometry = new THREE.BoxGeometry(2.8, 2.8, 2.8);
+var material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 });
+var rubicsCubeHitboxSouth = new THREE.Mesh(geometry, material);
+rubicsCubeHitboxSouth.visible = false;
+
+var material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 });
+var rubicsCubeHitboxEast = new THREE.Mesh(geometry, material);
+rubicsCubeHitboxEast.visible = false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+three.scene.add(cubeGroup);
+
+
+
+mainRotation(cubeGroup, three)
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio * 0.2); // Adjust the pixel ratio as needed
+document.body.appendChild(renderer.domElement);
+
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  three.camera.aspect = window.innerWidth / window.innerHeight;
+  three.camera.updateProjectionMatrix();
 }
 
-var testgeometry = new THREE.BoxGeometry(0.9,0.9, 0.9);
-var testmaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-
-var edges = new THREE.EdgesGeometry(testgeometry);
-edges.scale(0.95, 0.95, 0.95);
-
-var testcube = new THREE.Mesh(testgeometry, testmaterial);
-
-three.scene.add(testcube)
-testcube.position.set(0,0,4)
-
-
-
-
-
-
-
-
-three.scene.add(cubeGroup)
-
-
-
-
-
-
-
-const rotationSpeed = 0.01; 
-
-let isDragging = false;
-let previousMouseX = 0;
-let previousMouseY = 0;
-
-document.addEventListener('mousedown', onMouseDown, false);
-document.addEventListener('mousemove', onMouseMove, false);
-document.addEventListener('mouseup', onMouseUp, false);
-
-function onMouseDown(event) {
-  if (event.button === 0) { // Left mouse button
-    isDragging = true;
-    previousMouseX = event.clientX;
-    previousMouseY = event.clientY;
-  }
-}
-
-function onMouseMove(event) {
-  if (isDragging) {
-    const deltaX = event.clientX - previousMouseX;
-    const deltaY = event.clientY - previousMouseY;
-
-    cubeGroup.rotation.y += (deltaX * rotationSpeed);
-
-    // Restrict rotation along the X axis
-    const newXRotation = cubeGroup.rotation.x + (deltaY * rotationSpeed);
-    cubeGroup.rotation.x = Math.max(-Math.PI / 4, Math.min(Math.PI / 4, newXRotation));
-
-    previousMouseX = event.clientX;
-    previousMouseY = event.clientY;
-  }
-}
-
-function onMouseUp(event) {
-  if (event.button === 0) { // Left mouse button
-    isDragging = false;
-  }
-}
-
-
-
-//const composer = new POSTPROCESSING.EffectComposer(renderer);
-//composer.addPass(new POSTPROCESSING.RenderPass(scene, camera));
-//
-//const pixelationEffect = new POSTPROCESSING.PixelationEffect();
-//composer.addPass(pixelationEffect);
-//
-//pixelationEffect.resolution.set(512, 512);
-//pixelationEffect.pixelSize = 8.0;
-
-document.addEventListener("mouseover", mouseOverCube, false)
-
-
+window.addEventListener('resize', onWindowResize);
 
 
 function animate() {
   requestAnimationFrame(animate);
-  composer.render();
+  renderer.render(three.scene, three.camera);
 }
 animate();
---
+++ js/mainRotation.js
@@ -0,0 +1,75 @@
+
+
+
+export function mainRotation(cubeGroup, three) {
+    const rotationSpeed = 0.01;
+
+let isDragging = false;
+let previousMouseX = 0;
+let previousMouseY = 0;
+
+document.addEventListener('mousedown', onMouseDown, false);
+document.addEventListener('mousemove', onMouseMove, false);
+document.addEventListener('mouseup', onMouseUp, false);
+
+let isCursorOverCube = false; // Flag to track cursor position over the cube
+
+function checkCursorOverCube(event) {
+  // Convert mouse coordinates to normalized device coordinates
+  const mouse = new THREE.Vector2();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // Perform raycasting to check for intersection with the cube
+  const raycaster = new THREE.Raycaster();
+  raycaster.setFromCamera(mouse, three.camera);
+
+  const intersects = raycaster.intersectObjects(cubeGroup.children);
+
+  return intersects.length > 0;
+}
+
+function onMouseDown(event) {
+  if (event.button === 0) { // Left mouse button
+    if (!checkCursorOverCube(event)) {
+      isDragging = true;
+      previousMouseX = event.clientX;
+      previousMouseY = event.clientY;
+    }
+  }
+}
+
+function onMouseMove(event) {
+  if (isDragging) {
+    const deltaX = event.clientX - previousMouseX;
+    const deltaY = event.clientY - previousMouseY;
+
+    cubeGroup.rotation.y += (deltaX * rotationSpeed);
+
+    // Restrict rotation along the X axis
+    const newXRotation = cubeGroup.rotation.x + (deltaY * rotationSpeed);
+    cubeGroup.rotation.x = Math.max(-Math.PI / 3.5, Math.min(Math.PI / 3.5, newXRotation));
+
+    previousMouseX = event.clientX;
+    previousMouseY = event.clientY;
+  }
+}
+
+function onMouseUp(event) {
+  if (event.button === 0) { // Left mouse button
+    isDragging = false;
+  }
+}
+  }
+  
+  function getRandomColor() {
+    // Generate a random color
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  
